a066d57734b4a2e5cb6a4828d1f42efac517f90a
Samueelx <gsammygitau@gmail.com>
2021-07-27 01:15:06 +0300

    collection and submission of form data

--- js/script.js
+++ js/script.js
@@ -11,4 +11,16 @@ jQuery(document).ready( function() {
         $('.management img').toggle();
         $('.hidden-management').toggle();
     });
+
+    $('input').focus( function() {
+        $('button').show(1000, 'linear');
+    });
+
+    $('.contact-us form').submit( function(e) {
+        let name = $('input#name').val();
+        let email = $('input#email').val();
+        alert(name + ' thank you for contacting us. We have received your input.');
+
+        e.preventDefault();
+    });
 });
