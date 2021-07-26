c1752cb4152df95aa1479f3d5cb0124764dd6820
Samueelx <gsammygitau@gmail.com>
2021-07-26 22:20:59 +0300

    add toggle effect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 jQuery(document).ready( function() {
-    
+    $('.design').click( function() {
+        $('.design img').toggle();
+        $('.hidden-design').toggle();
+    });
+    $('.development').click( function() {
+        $('.development img').toggle();
+        $('.hidden-development').toggle();
+    });
+    $('.management').click( function() {
+        $('.management img').toggle();
+        $('.hidden-management').toggle();
+    });
 });
